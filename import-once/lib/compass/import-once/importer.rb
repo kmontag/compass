9f0c4a4f7f60765827c7bcc8d68208ea5a0dd4db
Kevin Montag <kmontag@cs.stanford.edu>
2014-11-12 12:01:24 -0800

    Fix nil engine returned by import-once importer

--- import-once/lib/compass/import-once/importer.rb
+++ import-once/lib/compass/import-once/importer.rb
@@ -8,6 +8,7 @@ module Compass
       end
 
       def find(uri, options, *args)
+        uri = uri.gsub(/^\(NOT IMPORTED\) /, '')
         uri, force_import = handle_force_import(uri)
         maybe_replace_with_dummy_engine(super(uri, options, *args), options, force_import)
       end
